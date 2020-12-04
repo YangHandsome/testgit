012a8402ea2ec68dba3c4b6d63cd325c16e5467d
yangpenghao <yangpenghao002@ke.com>
2020-12-04 17:18:24 +0800

    master test -1

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 文件描述
+ * @Author: yangpenghao002
+ * @Date: 2020-11-27 11:47:18
+ * @LastEditTime: 2020-11-27 14:42:14
+ * @LastEditors: yangpenghao002
+ */
+function test () {
+  console.log('test')
+  console.log(123)
+}
+test()
